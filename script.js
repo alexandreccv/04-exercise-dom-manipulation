4b03b150032fcd4c3b7d0da55c6577c90546fafd
Alexandre Carvalho <alex380bb@gmail.com>
2023-05-12 10:11:44 -0300

    exercicio 5 DOM

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.getElementById("paragrafo").innerText = "Daqui a dois anos quero estar 
 document.querySelector(".main-content").style.backgroundColor = "rgb(76, 164, 109)";
 document.querySelector(".main-content .center-content").style.backgroundColor = "white";
 document.getElementsByClassName("title")[0].innerText = "Desafio - JavaScript";
+document.getElementById("maiusculas").innerText = document.getElementById("maiusculas").innerText.toUpperCase();
 
 
 

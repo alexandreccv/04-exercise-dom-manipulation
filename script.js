33fc2712c9f3d21865b5a5b2f2dbe657bd4cdbd2
Alexandre Carvalho <alex380bb@gmail.com>
2023-05-12 09:55:02 -0300

    exercicio 3 DOM

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 document.getElementById("paragrafo").innerText = "Daqui a dois anos quero estar trabalhando com programação";
 document.querySelector(".main-content").style.backgroundColor = "rgb(76, 164, 109)";
+document.querySelector(".main-content .center-content").style.backgroundColor = "white";
 
 
